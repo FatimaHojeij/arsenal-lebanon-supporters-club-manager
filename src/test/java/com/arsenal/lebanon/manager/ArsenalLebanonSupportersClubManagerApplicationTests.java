5f31dc44e180b775e0bf5dc5a4347f707bcf3c65
FatimaHojeij <fatimahojeij97@gmail.com>
2026-06-23 00:22:05 +0300

    Initial commit

---
+++ src/test/java/com/arsenal/lebanon/manager/ArsenalLebanonSupportersClubManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsenal.lebanon.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsenalLebanonSupportersClubManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
